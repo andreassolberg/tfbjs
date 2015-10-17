10387516e2d882eb93ec6010d198d22f95355735
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2015-10-17 19:41:05 +0200

    Initial commit

---
+++ TFB.js
@@ -0,0 +1,147 @@
+var fs = require('fs');
+var cheerio = require('cheerio');
+var request = require('request');
+var querystring = require('querystring')
+
+
+
+if (typeof String.prototype.startsWith !== 'function') {
+	// see below for better implementation!
+	String.prototype.startsWith = function (str){
+		return this.indexOf(str) === 0;
+	};
+}
+
+
+var TFB = function(config) {
+	
+	this.jar = request.jar();
+	this.config = {
+		"sourceid": "mappami",
+		"mode": "login",
+		"lnr": config.username,
+		"passord": config.password,
+		"hskmg": "1"
+	};
+
+};
+
+TFB.prototype.login = function() {
+	
+	var that = this;
+	return new Promise(function(resolve, reject) {
+
+
+		var url = 'https://www.tfb.no/cgi-bin/mappami';
+		// console.log("that.config", that.config);
+		request.post({
+			"jar": that.jar,
+			"uri": url,
+			"headers": {
+				'content-type': 'application/x-www-form-urlencoded'
+			},
+			"body": querystring.stringify(that.config)
+		}, function (error, response, body) {
+
+			if (!error) {
+				resolve();
+			} else {
+				reject(error);
+			}
+
+		});
+
+
+	});
+
+}
+
+TFB.prototype.performListRequeset = function() {
+
+	var that = this;
+	return new Promise(function(resolve, reject) {
+
+		var url = 'https://www.tfb.no/cgi-bin/mappami?mode=vislaan';
+		request({
+				"url": url,
+				"jar": that.jar,
+				"headers": {
+					// 'cookie': 'sesjid=2015242072744-176934410900312677366440015549976577573413484531184; style=S',
+					'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36',
+					'referer': 'https://www.tfb.no/cgi-bin/mappami?mode=vislaan'
+				}
+			}, function (error, response, body) {
+
+
+				if (error) {
+					return reject()
+				}
+
+				// console.log("status code ", response.statusCode);
+
+				var $ = cheerio.load(body);
+
+				var data = [];
+				var tsnow = (new Date()).getTime();
+				var dr = /(\d{2})\/(\d{2})\/(\d{4})/;
+
+				$('.tabellUtlaan tbody tr').each(function(i, elem) {
+					
+					var e = $(this);
+					// console.log("-------");
+					// console.log();
+					var x = {};
+					x.cover = e.find('td').eq(0).find('img').eq(0).attr('src');
+					x.title = e.find('td').eq(1).find('a').last().text().trim();
+					x.deadlinetxt = e.find('td').eq(3).text().trim();
+					x.extended = e.find('td').eq(4).text().trim().startsWith('Ja');
+					
+					var p = dr.exec(x.deadlinetxt);
+					var pnd = p[3] + '-' + p[2] + '-' + p[1];
+					x.p = pnd;
+					x.deadline = new Date(pnd);
+					x.days = Math.ceil((x.deadline.getTime() -  tsnow) / (1000*3600*24));
+					// x.deadline = $(this).find('td[3]').text();
+					data.push(x);
+				});
+
+				// fs.writeFile("/tmp/bibbody.html", body, function(err) {
+				//     if(err) {
+				//         return console.log(err);
+				//     }
+
+				//     console.log("The file was saved!");
+				// }); 
+
+
+				return resolve(data);
+
+
+
+			});
+
+
+		});
+
+}
+
+
+TFB.prototype.getList = function() {
+	var that = this;
+	return this.login()
+		.then(function() {
+			return that.performListRequeset();
+		});
+
+}
+
+
+
+
+
+
+
+exports.TFB = TFB;
+
+
+
