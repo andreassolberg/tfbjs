9e5e97105dda5c47ac6d7ad185519710e627ad8e
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2015-10-31 09:18:27 +0100

    Add more debug handling in test script

--- test.js
+++ test.js
@@ -3,32 +3,26 @@ var TFB = require('./TFB').TFB;
 
 
 var config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+console.log(config);
 var b = new TFB(config);
-
-
 b.getList()
-	.then(function(data) {
-		// console.log("--- data ");
-		// console.log(data);
+    .then(function(data) {
+        console.log("--- data ");
+        console.log(data);
+
+        fs.writeFile("./bib.json", JSON.stringify(data, undefined, 2), function(err) {
+            if (err) {
+                return console.log(err);
+            }
+        });
 
 
-		fs.writeFile("./bib.json", JSON.stringify(data, undefined, 2), function(err) {
-		    if(err) {
-		        return console.log(err);
-		    }
-		}); 
-
-
-	});
-
-// console.log(config);
-
+    })
+    .catch(function(err) {
+        console.error("---- ERROR ----");
+        console.error(err.stack);
+    });
 
 
 
 exports.TFB = TFB;
-
-
-
-
-
