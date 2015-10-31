c190bd3f0251baac72bafe14c009427fdf77c8d4
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2015-10-31 07:38:42 +0100

    Adding username in results

--- TFB.js
+++ TFB.js
@@ -7,14 +7,14 @@ var querystring = require('querystring')
 
 if (typeof String.prototype.startsWith !== 'function') {
 	// see below for better implementation!
-	String.prototype.startsWith = function (str){
+	String.prototype.startsWith = function(str) {
 		return this.indexOf(str) === 0;
 	};
 }
 
 
 var TFB = function(config) {
-	
+
 	this.jar = request.jar();
 	this.config = {
 		"sourceid": "mappami",
@@ -27,7 +27,7 @@ var TFB = function(config) {
 };
 
 TFB.prototype.login = function() {
-	
+
 	var that = this;
 	return new Promise(function(resolve, reject) {
 
@@ -40,7 +40,7 @@ TFB.prototype.login = function() {
 				'content-type': 'application/x-www-form-urlencoded'
 			},
 			"body": querystring.stringify(that.config)
-		}, function (error, response, body) {
+		}, function(error, response, body) {
 
 			if (!error) {
 				resolve();
@@ -62,56 +62,57 @@ TFB.prototype.performListRequeset = function() {
 
 		var url = 'https://www.tfb.no/cgi-bin/mappami?mode=vislaan';
 		request({
-				"url": url,
-				"jar": that.jar,
-				"headers": {
-					// 'cookie': 'sesjid=xxxxxx; style=S',
-					'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36',
-					'referer': 'https://www.tfb.no/cgi-bin/mappami?mode=vislaan'
-				}
-			}, function (error, response, body) {
+			"url": url,
+			"jar": that.jar,
+			"headers": {
+				// 'cookie': 'sesjid=xxxxxx; style=S',
+				'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36',
+				'referer': 'https://www.tfb.no/cgi-bin/mappami?mode=vislaan'
+			}
+		}, function(error, response, body) {
 
 
-				if (error) {
-					return reject()
-				}
+			if (error) {
+				return reject()
+			}
 
-				// console.log("status code ", response.statusCode);
+			// console.log("status code ", response.statusCode);
 
-				var $ = cheerio.load(body);
+			var $ = cheerio.load(body);
 
-				var data = [];
-				var tsnow = (new Date()).getTime();
-				var dr = /(\d{2})\/(\d{2})\/(\d{4})/;
+			var data = [];
+			var tsnow = (new Date()).getTime();
+			var dr = /(\d{2})\/(\d{2})\/(\d{4})/;
 
-				$('.tabellUtlaan tbody tr').each(function(i, elem) {
-					
-					var e = $(this);
-					// console.log("-------");
-					// console.log();
-					var x = {};
-					x.cover = e.find('td').eq(0).find('img').eq(0).attr('src');
-					x.title = e.find('td').eq(1).find('a').last().text().trim();
-					x.deadlinetxt = e.find('td').eq(3).text().trim();
-					x.extended = e.find('td').eq(4).text().trim().startsWith('Ja');
-					
-					var p = dr.exec(x.deadlinetxt);
-					var pnd = p[3] + '-' + p[2] + '-' + p[1];
-					x.p = pnd;
-					x.deadline = new Date(pnd);
-					x.days = Math.ceil((x.deadline.getTime() -  tsnow) / (1000*3600*24));
-					// x.deadline = $(this).find('td[3]').text();
-					data.push(x);
-				});
+			$('.tabellUtlaan tbody tr').each(function(i, elem) {
 
-				return resolve(data);
+				var e = $(this);
+				// console.log("-------");
+				// console.log();
+				var x = {};
+				x.cover = e.find('td').eq(0).find('img').eq(0).attr('src');
+				x.title = e.find('td').eq(1).find('a').last().text().trim();
+				x.deadlinetxt = e.find('td').eq(3).text().trim();
+				x.extended = e.find('td').eq(4).text().trim().startsWith('Ja');
+				x.username = that.config.lnr;
 
-
-
+				var p = dr.exec(x.deadlinetxt);
+				var pnd = p[3] + '-' + p[2] + '-' + p[1];
+				x.p = pnd;
+				x.deadline = new Date(pnd);
+				x.days = Math.ceil((x.deadline.getTime() - tsnow) / (1000 * 3600 * 24));
+				// x.deadline = $(this).find('td[3]').text();
+				data.push(x);
 			});
+
+			return resolve(data);
+
 
 
 		});
+
+
+	});
 
 }
 
@@ -127,11 +128,4 @@ TFB.prototype.getList = function() {
 
 
 
-
-
-
-
 exports.TFB = TFB;
-
-
-
